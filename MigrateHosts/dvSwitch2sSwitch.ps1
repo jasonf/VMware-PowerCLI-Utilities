19b2dc011ba7bf1b1b240c33bb766fdf635ff8ab
Jason Fiset <jafiset@gmail.com>
2011-08-04 15:30:02 -0700

    Filter results for menu based on previous selections

--- MigrateHosts/dvSwitch2sSwitch.ps1
+++ MigrateHosts/dvSwitch2sSwitch.ps1
@@ -10,4 +10,6 @@ Connect-VIServer $vCenterCurrent
 
 $datacenter = Menu (Get-Datacenter | sort) "Select the datacenter to modify"
 
-$cluster = Menu (Get-Cluster | sort ) "Select the Cluster to modify"
+$cluster = Menu ($datacenter | Get-Cluster | sort) "Select the Cluster to modify"
+
+$host = Menu ($cluster | Get-VMhost | sort) "Select the Host to modify"
